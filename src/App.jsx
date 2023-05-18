af1f4332231c3cf144546d5a791906965a5e25aa
术生何 <urfdvw@gmail.com>
2023-05-18 10:58:46 -0700

    saperate title

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,41 @@ import ScrollableFeed from 'react-scrollable-feed'
 // https://stackoverflow.com/a/52673227/7037749
 import useSerial from "./useSerial";
 
-const SerialConsole = () => {
+const TITLE_START = '\x1B]0;';
+const TITLE_END = '\x1B\\';
+const CTRL_C = "\x03";
+const CTRL_D = "\x04";
+const LINE_END = '\x0D';
+
+const latestTitle = (text) => {
+    return text.split(TITLE_START).at(-1).split(TITLE_END).at(0);
+}
+
+const removeTitle = (text) => {
+    return text.split(TITLE_START).map(x => x.split(TITLE_END).at(1)).join('')
+}
+
+const App = () => {
 
     const { connect, disconnect, sendData, output, connected } = useSerial();
     const [input, setInput] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        sendData(input + '\x0D');
+        sendData(input + LINE_END);
         setInput('');
     };
 
     return (
         <div>
             <h2>Serial Console</h2>
-            {connected ? (
-                <Button variant="contained" onClick={disconnect}>Disconnect</Button>
-            ) : (
+            <h3>{latestTitle(output)}</h3>
+            {!connected && (
                 <Button variant="contained" onClick={connect}>Connect</Button>
             )}
             <div style={{ "max-height": '350pt' }}>
                 <ScrollableFeed>
-                    <pre>{output}</pre>
+                    <pre>{removeTitle(output)}</pre>
                 </ScrollableFeed>
             </div>
             {connected && (
@@ -41,10 +54,10 @@ const SerialConsole = () => {
                         <Button variant="contained" type="submit">Send</Button>
                     </form>
                     <Button variant="contained"
-                        onClick={() => { sendData("\x03") }}
+                        onClick={() => { sendData(CTRL_C) }}
                     >Ctrl-C</Button>
                     <Button variant="contained"
-                        onClick={() => { sendData("\x04") }}
+                        onClick={() => { sendData(CTRL_D) }}
                     >Ctrl-D</Button>
                 </>
             )}
@@ -52,4 +65,4 @@ const SerialConsole = () => {
     );
 };
 
-export default SerialConsole;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SerialConsole from './App'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SerialConsole />
+    <App />
   </React.StrictMode>,
 )
