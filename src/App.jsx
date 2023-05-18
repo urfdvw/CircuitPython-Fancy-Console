1368be923b965b7aaf60ffb873aa8680563cd0e3
术生何 <urfdvw@gmail.com>
2023-05-17 21:58:09 -0700

    move serial logic to hook
    disconnect is not working

--- src/App.jsx
+++ src/App.jsx
@@ -3,79 +3,12 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import ScrollableFeed from 'react-scrollable-feed'
 // https://stackoverflow.com/a/52673227/7037749
+import useSerial from "./useSerial";
 
 const SerialConsole = () => {
-    const [port, setPort] = useState(null);
-    const [output, setOutput] = useState('');
+
+    const { connect, disconnect, sendData, output, connected } = useSerial();
     const [input, setInput] = useState('');
-
-    useEffect(() => {
-        if (!navigator.serial) {
-            console.warn('Web Serial API not supported');
-        }
-    }, []);
-
-    useEffect(() => {
-        let reader;
-        let active = true;
-
-        const readData = async () => {
-            while (port && port.readable && active) {
-                try {
-                    reader = port.readable.getReader();
-                    while (true) {
-                        const { value, done } = await reader.read();
-                        if (done) break;
-                        setOutput((prevOutput) => prevOutput.slice(-100000) + new TextDecoder().decode(value));
-                    }
-                } catch (err) {
-                    console.error('Failed to read data:', err);
-                } finally {
-                    reader && reader.releaseLock();
-                }
-            }
-        };
-
-        readData();
-
-        return () => {
-            active = false;
-            reader && reader.releaseLock();
-        };
-    }, [port]);
-
-    const connect = async () => {
-        try {
-            const newPort = await navigator.serial.requestPort();
-            await newPort.open({ baudRate: 115200 });
-            setPort(newPort);
-        } catch (err) {
-            console.error('Failed to connect:', err);
-        }
-    };
-
-    const sendData = async (data) => {
-        if (!port || !port.writable) return;
-
-        const writer = port.writable.getWriter();
-        const encodedData = new TextEncoder().encode(data);
-
-        try {
-            await writer.write(encodedData);
-        } catch (err) {
-            console.error('Failed to send data:', err);
-        } finally {
-            writer.releaseLock();
-        }
-    };
-
-    const disconnect = async () => {
-        if (!port) return;
-
-        await port.close();
-        setPort(null);
-        setOutput('');
-    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -86,7 +19,7 @@ const SerialConsole = () => {
     return (
         <div>
             <h2>Serial Console</h2>
-            {port ? (
+            {connected ? (
                 <Button variant="contained" onClick={disconnect}>Disconnect</Button>
             ) : (
                 <Button variant="contained" onClick={connect}>Connect</Button>
@@ -96,7 +29,7 @@ const SerialConsole = () => {
                     <pre>{output}</pre>
                 </ScrollableFeed>
             </div>
-            {port && (
+            {connected && (
                 <>
                     <form onSubmit={handleSubmit}>
                         <TextField
---
+++ src/useSerial.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+
+const useSerial = () => {
+  const [port, setPort] = useState(null);
+  const [output, setOutput] = useState("");
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    if (!navigator.serial) {
+      console.warn("Web Serial API not supported");
+    }
+  }, []);
+
+  useEffect(() => {
+    let reader;
+    let active = true;
+
+    const readData = async () => {
+      while (port && port.readable && active) {
+        try {
+          reader = port.readable.getReader();
+          while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+              reader.releaseLock();
+              break;
+            }
+            setOutput(
+              (prevOutput) =>
+                prevOutput.slice(-100000) + new TextDecoder().decode(value)
+            );
+          }
+        } catch (err) {
+          console.error("Failed to read data:", err);
+        } finally {
+          reader && reader.releaseLock();
+        }
+      }
+    };
+
+    readData();
+
+    return () => {
+      active = false;
+      reader && reader.releaseLock();
+    };
+  }, [port]);
+
+  const connect = async () => {
+    try {
+      const newPort = await navigator.serial.requestPort();
+      await newPort.open({ baudRate: 115200 });
+      setPort(newPort);
+      setConnected(true);
+    } catch (err) {
+      console.error("Failed to connect:", err);
+    }
+  };
+
+  const sendData = async (data) => {
+    if (!port || !port.writable) return;
+
+    const writer = port.writable.getWriter();
+    const encodedData = new TextEncoder().encode(data);
+
+    try {
+      await writer.write(encodedData);
+    } catch (err) {
+      console.error("Failed to send data:", err);
+    } finally {
+      writer.releaseLock();
+    }
+  };
+
+  const disconnect = async () => {
+    if (!port) return;
+
+    const reader = port.readable.getReader();
+    // Close the input stream (reader).
+    if (reader) {
+      await reader.cancel();
+    }
+
+    await port.close();
+    setPort(null);
+    setConnected(false);
+    setOutput("");
+  };
+
+  return { connect, disconnect, sendData, output, connected };
+};
+
+export default useSerial;
