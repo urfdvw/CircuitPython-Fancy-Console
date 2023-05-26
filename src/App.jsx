a211eea49830da9f38fb2ac675845f5588d220ae
术生何 <urfdvw@gmail.com>
2023-05-25 23:08:44 -0400

    move to module

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Autocomplete from '@mui/material/Autocomplete';
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -23,6 +22,7 @@ import {
 import * as constants from "./constants";
 // Mine widgets
 import { VariableDisp, CreateVariableDisp } from "./VariableDisp";
+import VariableSet from "./VariableSet"
 
 const App = () => {
   // Hooks --------------------------------------
@@ -50,7 +50,7 @@ const App = () => {
     },
     {
       key: "fkjvhgifvbhgsjd",
-      type: "VariableSetInt",
+      type: "VariableSet",
       variableName: "a",
       displayName: "Change `a` to",
       windowed: false,
@@ -69,9 +69,9 @@ const App = () => {
           displayName={wid.displayName}
         />
       );
-    } else if (wid.type === "VariableSetInt") {
+    } else if (wid.type === "VariableSet") {
       return (
-        <VariableSetInt
+        <VariableSet
           connectedVariables={connectedVariables}
           sendData={sendData}
         />
@@ -251,89 +251,6 @@ const App = () => {
         setWidgets={setWidgets}
       />
     </div>
-  );
-};
-
-const VariableSetInt = ({
-  connectedVariables,
-  sendData,
-}) => {
-  const [variableName, setVariableName] = useState('');
-  const [value, setValue] = useState(0);
-  const [type, setType] = useState("int");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    let variableValue;
-    if (type === "int") {
-      variableValue = parseInt(value);
-    } else if (type === "float") {
-      variableValue = parseFloat(value);
-    } else if (type === "string") {
-      variableValue = String(value);
-    } else if (type === "json") {
-      try {
-        variableValue = JSON.parse(value);
-      } catch {
-        alert('Input is not a valid json');
-      }
-    }
-    const updatedVariable = { [variableName]: variableValue }; //https://stackoverflow.com/a/29077216/7037749
-    sendData(
-      constants.CV_JSON_START +
-      JSON.stringify(updatedVariable) +
-      constants.CV_JSON_END +
-      constants.LINE_END
-    );
-    // don't up date variable value on web directly, let the change reflected by update echo
-  };
-  console.log(
-    Object.keys(connectedVariables)
-  )
-  return (
-    <>
-      <Autocomplete
-        disableClearable
-        id="combo-box-demo"
-        value={variableName}
-        onChange={(e, newValue) => {
-          setVariableName(newValue.label);
-        }}
-        options={
-          Object.keys(connectedVariables).map(key => {
-            return {
-              label: key
-            }
-          })
-        }
-        sx={{ width: 100 }}
-        renderInput={(params) => <TextField {...params} label="Variable" />}
-      />
-      <TextField
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-        label="value"
-      ></TextField>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">type</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          value={type}
-          label="type"
-          onChange={(e) => {
-            setType(e.target.value);
-          }}
-        >
-          <MenuItem value={"int"}>int</MenuItem>
-          <MenuItem value={"float"}>float</MenuItem>
-          <MenuItem value={"string"}>string</MenuItem>
-          <MenuItem value={"json"}>json</MenuItem>
-        </Select>
-      </FormControl>
-      <Button variant="contained" onClick={handleSubmit}>Set</Button>
-    </>
   );
 };
 
---
+++ src/VariableSet.jsx
@@ -0,0 +1,94 @@
+
+import React, { useState } from "react";
+import Autocomplete from '@mui/material/Autocomplete';
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import * as constants from "./constants";
+
+const VariableSet = ({
+    connectedVariables,
+    sendData,
+}) => {
+    const [variableName, setVariableName] = useState('');
+    const [value, setValue] = useState(0);
+    const [type, setType] = useState("int");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let variableValue;
+        if (type === "int") {
+            variableValue = parseInt(value);
+        } else if (type === "float") {
+            variableValue = parseFloat(value);
+        } else if (type === "string") {
+            variableValue = String(value);
+        } else if (type === "json") {
+            try {
+                variableValue = JSON.parse(value);
+            } catch {
+                alert('Input is not a valid json');
+            }
+        }
+        const updatedVariable = { [variableName]: variableValue }; //https://stackoverflow.com/a/29077216/7037749
+        sendData(
+            constants.CV_JSON_START +
+            JSON.stringify(updatedVariable) +
+            constants.CV_JSON_END +
+            constants.LINE_END
+        );
+        // don't up date variable value on web directly, let the change reflected by update echo
+    };
+    console.log(
+        Object.keys(connectedVariables)
+    )
+    return (
+        <>
+            <Autocomplete
+                disableClearable
+                id="combo-box-demo"
+                value={variableName}
+                onChange={(e, newValue) => {
+                    setVariableName(newValue.label);
+                }}
+                options={
+                    Object.keys(connectedVariables).map(key => {
+                        return {
+                            label: key
+                        }
+                    })
+                }
+                sx={{ width: 100 }}
+                renderInput={(params) => <TextField {...params} label="Variable" />}
+            />
+            <TextField
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value);
+                }}
+                label="value"
+            ></TextField>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    value={type}
+                    label="type"
+                    onChange={(e) => {
+                        setType(e.target.value);
+                    }}
+                >
+                    <MenuItem value={"int"}>int</MenuItem>
+                    <MenuItem value={"float"}>float</MenuItem>
+                    <MenuItem value={"string"}>string</MenuItem>
+                    <MenuItem value={"json"}>json</MenuItem>
+                </Select>
+            </FormControl>
+            <Button variant="contained" onClick={handleSubmit}>Set</Button>
+        </>
+    );
+};
+export default VariableSet
