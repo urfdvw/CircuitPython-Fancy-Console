940bae6ab751187b4bfbb7f97f9ffc03ec135d5b
术生何 <urfdvw@gmail.com>
2023-05-27 12:28:37 -0400

    Update connected_variables.py

--- CIRCUITPY/connected_variables.py
+++ CIRCUITPY/connected_variables.py
@@ -1,96 +1,85 @@
-import json, sys, supervisor, time
-from matcher import BracketMatcher, MatcherProcessor
+import { useState } from "react";
 
-CV_JSON_START = "<CV>"
-CV_JSON_END = "</CV>"
-LINE_END = "\n"
-UPDATE_PERIOD = 3
+const useFileSystem = () => {
+  const [directoryHandle, setDirectoryHandle] = useState(null);
 
-class ConnectedVariables:
-    def __init__(self):
-        self.vars = {}
-        self.cv_processor = MatcherProcessor(
-            BracketMatcher(CV_JSON_START, CV_JSON_END),
-            exit_action=self.exit_action
-        )
-        self.last_time_stamp = time.monotonic()
+  async function openDirectory() {
+    setDirectoryHandle(await window.showDirectoryPicker());
+  }
 
-    def heart_beat(self):
-        if time.monotonic() - self.last_time_stamp > UPDATE_PERIOD:
-            print(CV_JSON_START + json.dumps(self.vars) + CV_JSON_END, end='')
-            self.last_time_stamp = time.monotonic()
+  async function fileHandle2text(fileHandle) {
+    try {
+      const file = await fileHandle.getFile();
+      const contents = await file.text();
+      return String(contents);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
-    def exit_action (self, text, branch):
-        try:
-            # parse
-            serial_updates_dict = json.loads(branch.strip())
-            # check
-            assert all(
-                [key in self.vars for key in serial_updates_dict]
-            ), "get unknown variable names from serial"
-            # cast type 
-            for key, value in serial_updates_dict.items():
-                serial_updates_dict[key] = type(self.vars[key])(value)
-            # echo update
-            print(CV_JSON_START + json.dumps(serial_updates_dict) + CV_JSON_END, end='')
-            # update
-            self.vars.update(serial_updates_dict)
-        except Exception as e:
-            self.serial_updates = ""  # clean up bad data
-            print(e)
+  async function path2Handles(path, opt) {
+    if (!opt) {
+      opt = {};
+    }
+    // change windows path to the world standard
+    path.replace("\\", "/");
+    // split path to levels
+    const levels = path.split("/").map((l) => l.trim());
+    // get dir handle
+    let curDirectoryHandle = directoryHandle;
+    for (const l of levels.slice(0, -1)) {
+      console.log("l", [l]);
+      curDirectoryHandle = await curDirectoryHandle.getDirectoryHandle(l);
+    }
+    // get file handle
+    const fileHandle =
+      levels.at(-1) === ""
+        ? null
+        : await curDirectoryHandle.getFileHandle(levels.at(-1), opt);
+    return [curDirectoryHandle, fileHandle];
+  }
 
-    def define(self, var_name, initdata):
-        """
-        define a connected variable
-        """
-        assert type(var_name) == type(""), "var_name should be string"
-        self.vars[var_name] = initdata
+  async function readFile(path, opt) {
+    try {
+      const [curDirectoryHandle, fileHandle] = await path2Handles(path, opt);
+      console.log(path, "found");
+      return fileHandle2text(fileHandle);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
-    def serial_read(self):
-        """
-        read CDC data from serial buffer if any
-        don't use input() in other parts of the code
-        """
-        # read from serial
-        while n_bytes := supervisor.runtime.serial_bytes_available:
-            self.cv_processor.push([sys.stdin.read(n_bytes)])
-            time.sleep(0.01)
+  async function readDir(path) {
+    try {
+      const [curDirectoryHandle, fileHandle] = await path2Handles(path);
+      console.log(path, "found");
+      return await curDirectoryHandle.entries();
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
-    def read(self, var_names):
-        """
-        read a defined variable or a list of defined variables
-        """
-        # type enforce
-        ## convert the input to a list if it is not
-        if type(var_names) != type([]):
-            var_names = [var_names]
-        assert all(
-            [type(name) == type("") for name in var_names]
-        ), "all names should be strings"
-        # get updates
-        self.serial_read()
-        # get values
-        output = [self.vars[name] for name in var_names]
-        if len(output) == 1:
-            return output[0]
-        return output
+  async function writeFile(path, text) {
+    try {
+      const [curDirectoryHandle, fileHandle] = await path2Handles(path, {
+        create: true
+      });
+      // Create a FileSystemWritableFileStream to write to.
+      const writable = await fileHandle.createWritable();
+      // Write the contents of the file to the stream.
+      await writable.write(text);
+      // Close the file and write the contents to disk.
+      await writable.close();
+      console.log("Successfully wrote to", path);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-    def write(self, var_names, var_values):
-        # type enforce
-        if type(var_names) != type([]):
-            var_names = [var_names]
-            var_values = [var_values]
-        assert all(
-            [type(name) == type("") for name in var_names]
-        ), "all names should be strings in python"
-        assert all(
-            [
-                type(self.vars[name]) == type(value)
-                for name, value in zip(var_names, var_values)
-            ]
-        ), "variable type does not match in python"
-        # update
-        updates_dict = {name: value for name, value in zip(var_names, var_values)}
-        self.vars.update(updates_dict)
-        # send updates
-        print(CV_JSON_START + json.dumps(updates_dict) + CV_JSON_END, end='')
+  return { openDirectory, readFile, readDir, writeFile };
+};
+
+export default useFileSystem;
