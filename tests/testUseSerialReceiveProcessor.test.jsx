9ad657fef680eb7c9b7a53fd52edd78dbc10734d
术生何 <urfdvw@gmail.com>
2023-06-26 21:24:56 -0400

    strange mac symbol

--- tests/testUseSerialReceiveProcessor.test.jsx
+++ tests/testUseSerialReceiveProcessor.test.jsx
@@ -100,7 +100,6 @@ describe('test block breaking', () => {
         ]);
     });
     const scenario_break_repl = "\u001b]0;🐍Wi-Fi: off | REPL | 8.0.5\u001b\\\r\n>>> \r\n\u001b]0;🐍Wi-Fi: off | Done | 8.0.5\u001b\\soft reboot\r\n\r\nAuto-reload is on. Simply save files over USB to run them or enter REPL to disable.\r\ncode.py output:\r\n\u001b]0;🐍Wi-Fi: off | code.py | 8.0.5\u001b\\0\r\n1\r\n2\r\n3\r\n4\r\nTraceback (most recent call last):\r\n  File \"code.py\", line 11, in <module>\r\nKeyboardInterrupt: \r\n\u001b]0;🐍Wi-Fi: off | 11@code.py KeyboardInterrupt | 8.0.5\u001b\\\r\nCode done running.\r\n\r\nPress any key to enter the REPL. Use CTRL-D to reload.\r\n\u001b]0;🐍Wi-Fi: off | REPL | 8.0.5\u001b\\\r\nAdafruit CircuitPython 8.0.5 on 2023-03-31; Adafruit Feather ESP32S2 with ESP32S2\r\n>>> a = 1\r\n>>> a\r\n1\r\n>>> "
-ß
     it('scenario_break_repl', () => {
         expect(output_to_blocks(scenario_break_repl)).toStrictEqual([
             {
