79b145a3c8a97c166069ead702641c230226da7f
urfdvw <urfdvw@gmail.com>
2023-06-29 10:01:07 -0400

    repl component

--- src/FancyConsole.jsx
+++ src/FancyConsole.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import { useSerialReceiveProcessor } from "./useSerialReceiveProcessor"
+import { useSerialReceiveProcessor, body_text_to_repl_conversation } from "./useSerialReceiveProcessor"
 
 const widgetStyles = {
     bgcolor: "background.paper",
@@ -12,17 +12,53 @@ const widgetStyles = {
     minHeight: "40px", // for the size of delet icon
 };
 
+export const Repl = ({ body }) => {
+    if (!body) {
+        return <></>
+    }
+    const repl_conversation = body_text_to_repl_conversation(body);
+    console.log(repl_conversation)
+    return (
+        <>
+        {
+            <pre style={{ whiteSpace: "pre-wrap" }}>
+                {repl_conversation.info}
+            </pre>
+        }
+        {
+            repl_conversation.conversation.map(block => {
+                return <>
+                    {block.code ? <Box sx={widgetStyles}>
+                        <pre style={{ whiteSpace: "pre-wrap" }}>
+                            {block.code}
+                        </pre>
+                    </Box> : <></>}
+                    {block.results ? <pre style={{ whiteSpace: "pre-wrap" }}>
+                        {block.results}
+                    </pre> : <></>}
+                </>
+            })
+        }
+        </>
+    )
+}
+
 export const FancyConsole = ({ output }) => {
     const { isCpy8, title, sessions } = useSerialReceiveProcessor(output);
-    useEffect(() => { console.log(sessions) }, [sessions])
+    // useEffect(() => { console.log(sessions) }, [sessions])
     return (<>
         {sessions.map(block => {
             return <>
                 {block.body ? <Box sx={widgetStyles}>
                     <p>{block.is_repl ? "REPL" : "Code"}</p>
-                    <pre style={{ whiteSpace: "pre-wrap" }}>
-                        {block.body}
-                    </pre>
+                    
+                        {
+                            block.is_repl
+                            ? <Repl body={block.body} />
+                            : <pre style={{ whiteSpace: "pre-wrap" }}>
+                                {block.body}
+                            </pre>
+                        }
                 </Box> : <></>}
                 {block.info ? <pre style={{ whiteSpace: "pre-wrap" }}>
                     {block.info}
