9bffd568939ae5efc0b5d5e06715c21817798737
urfdvw <urfdvw@gmail.com>
2023-06-29 18:34:08 -0400

    batch repl component test

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import App from '../component_tests/TestFancyConsole'
+// import App from '../component_tests/TestFancyConsole'
+import App from '../component_tests/TestRepl'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
