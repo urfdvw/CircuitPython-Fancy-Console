3360219dde4e13bb2bbf62043853d603cfb3a351
术生何 <urfdvw@gmail.com>
2023-06-28 22:26:24 -0400

    Moved component test file

--- componentTests/TestFancyConsole.jsx
+++ componentTests/TestFancyConsole.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { FancyConsole } from "./FancyConsole";
+import { FancyConsole } from "../src/FancyConsole";
 import Divider from '@mui/material/Divider';
 import { output_scenarios } from '../tests/testUseSerialReceiveProcessor.data'
 
 const App = () => {
     const scenarios = Object.keys(output_scenarios)
-    console.log(scenarios.map(name => output_scenarios[name].in))
     return (<>
         {scenarios.map(name => <>
             <p>{name}</p>
