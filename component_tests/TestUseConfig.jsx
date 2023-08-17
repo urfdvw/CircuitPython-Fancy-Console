18f80ae21590c60c274d54c08973d446f9daf922
River Wang <urfdvw@gmail.com>
2023-08-16 22:10:10 -0400

    json schema form test
    section
    omit unsupported fields

--- component_tests/TestUseConfig.jsx
+++ component_tests/TestUseConfig.jsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
 import log from "loglevel";
 import useConfig from "../src/useConfig";
+import schema from "./testConfigSchema.json";
 
 import validator from "@rjsf/validator-ajv8";
-import Form from '@rjsf/mui';
-
-
+import Form from "@rjsf/mui";
 
 log.setLevel("trace"); // show all
 
 const App = () => {
     useConfig();
-    const schema = {
-        title: "Test form",
-        type: "string",
+    const handleSubmit = (formData) => {
+        log.debug("Form data:", formData);
     };
-    return <Form schema={schema} validator={validator} />;
+    const form_data = { a: 100 }; //unsupported fields are omitted on submit
+    return (
+        <Form
+            schema={schema}
+            validator={validator}
+            formData={form_data}
+            onSubmit={(e) => {
+                handleSubmit(e.formData);
+                log.info("form submitted");
+            }}
+            omitExtraData={true}
+        />
+    );
 };
 
 export default App;
